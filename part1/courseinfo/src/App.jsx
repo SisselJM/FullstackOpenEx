57d34635d82fcfd623a1d923aee776a8ade9a56c
SisselJM <144883413+SisselJM@users.noreply.github.com>
2024-04-10 09:02:40 +0200

    1.5 1 object

--- part1/courseinfo/src/App.jsx
+++ part1/courseinfo/src/App.jsx
@@ -29,8 +29,9 @@ const Total = (props) => {
   )
 }
 const App = () => {
-  const course = 'Half Stack application development'
-  const parts = [
+  const course = {
+    name: 'Half Stack application development',
+    parts: [
     {
       name: 'Fundamentals of React',
       exercises: 10
@@ -44,11 +45,12 @@ const App = () => {
       exercises: 14
     }
   ]
+  };
   return (
     <div>
-      <Header title={course} />
-      <Content parts={parts} />
-      <Total parts={parts} />
+      <Header title={course.name} />
+      <Content parts={course.parts} />
+      <Total parts={course.parts} />
     </div>
   )
 }
