c1381a2d5757db3f310e0460e67dcb8d88933c14
SisselJM <144883413+SisselJM@users.noreply.github.com>
2024-04-10 12:27:40 +0200

    1.7 calculate statistics

--- part1/unicafe/src/App.jsx
+++ part1/unicafe/src/App.jsx
@@ -5,22 +5,52 @@ function App() {
   const [good, setGood] = useState(0)
   const [neutral, setNeutral] = useState(0)
   const [bad, setBad] = useState(0)
+  const [all, setAll] = useState(0)
+
+  const handleGoodClick = () => {
+    setGood(good + 1)
+    setAll(all + 1)
+  }
+
+  const handleNeutralClick = () => {
+    setNeutral(neutral + 1)
+    setAll(all + 1)
+  }
+
+  const handleBadClick = () => {
+    setBad(bad + 1)
+    setAll(all + 1)
+  }
+
+  const calculateAverage = () => {
+    if (all === 0) {
+      return 0 
+    }
+    return (good - bad) / all
+  }
+
+  const calculatePositiveFeedback = () => {
+    if (all === 0) {
+      return 0 
+    }
+    return 100 * (good / all)
+  }
 
   return (
     <div>
       <h1>Give feedback</h1>
       <p>
-        <button onClick={() => setGood(good + 1)}>good</button>
-        <button>neutral</button>
-        <button>bad</button>
+        <button onClick={handleGoodClick}>good</button>
+        <button onClick={handleNeutralClick}>neutral</button>
+        <button onClick={handleBadClick}>bad</button>
       </p>
       <h1>Statistics</h1>
       <p>Good {good}</p>
       <p>Neutral {neutral}</p>
       <p>Bad {bad}</p>
-      <p>All {good+neutral+bad}</p>
-      <p>Averge {good}</p>
-      <p>Positive {good}</p>
+      <p>All {all}</p>
+      <p>Averge {calculateAverage()}</p>
+      <p>Positive {calculatePositiveFeedback()} %</p>
     </div>
   )
 }
