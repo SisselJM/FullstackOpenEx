e45d47ed90bd2fe85597238a3d31893a6464a1a1
SisselJM <144883413+SisselJM@users.noreply.github.com>
2024-04-11 12:37:08 +0200

    2.10 ignore case

--- part2/phonebook/src/App.jsx
+++ part2/phonebook/src/App.jsx
@@ -62,21 +62,21 @@ function App() {
   }
 
   const handleNameChange = (event) => {
-    console.log(event.target.value)
+    //console.log(event.target.value)
     setNewName(event.target.value)
   }
 
   const handleNumberChange = (event) => {
-    console.log(event.target.value)
+    //console.log(event.target.value)
     setNewNumber(event.target.value)
   }
 
   const handleFilterChange = (event) => {
-    console.log(event.target.value)
+    //console.log(event.target.value)
     setFilterValue(event.target.value)
   }
 
-  const result = persons.filter(p => p.name.includes(filterValue))
+  const result = persons.filter(p => p.name.toLowerCase().includes(filterValue.toLowerCase()))
   //console.log('filtered: ', result)
 
   return (
