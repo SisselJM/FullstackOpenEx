81047f5950fe3568a29a4b40e1a8bdac9d50e23c
SisselJM <144883413+SisselJM@users.noreply.github.com>
2024-04-11 15:41:14 +0200

    2.11 get data from db.json

--- part2/phonebook/src/App.jsx
+++ part2/phonebook/src/App.jsx
@@ -1,4 +1,5 @@
-import { useState } from 'react'
+import { useEffect, useState } from 'react'
+import axios from 'axios'
 
 const Person = ({ person }) => <p>{person.name} {person.number}</p>
 
@@ -34,15 +35,20 @@ const Persons = ({persons}) => {
 }
 
 function App() {
-  const [persons, setPersons] = useState([
-    { name: 'Arto Hellas', number: '12345678', id: 1 },
-    { name: 'Ada Lovelace', number: '39-44-5323523', id: 2 },
-    { name: 'Dan Abramov', number: '12-43-234345', id: 3 },
-    { name: 'Mary Poppendieck', number: '39-23-6423122', id: 4 }
-  ]) 
+  const [persons, setPersons] = useState([]) 
   const [newName, setNewName] = useState('')
   const [newNumber, setNewNumber] = useState('')
   const [filterValue, setFilterValue] = useState('')
+
+  useEffect(() => {
+    //console.log('effect')
+    axios
+      .get('http://localhost:3001/persons')
+      .then(reponse => {
+        //console.log('promise')
+        setPersons(reponse.data)
+      })
+  }, [])
 
   const addPerson = (event) => {
     event.preventDefault()
