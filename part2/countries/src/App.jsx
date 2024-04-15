d2bf888f73eef82297296c84a71d19b8e49c257d
SisselJM <144883413+SisselJM@users.noreply.github.com>
2024-04-15 08:34:26 +0200

    Get countries from rest api

--- part2/countries/src/App.jsx
+++ part2/countries/src/App.jsx
@@ -1,12 +1,25 @@
 import { useState } from 'react'
+import countriesService from './services/countries'
 
 function App() {
   const [searchName, setSearchName] = useState('')
+  const searchCountries = (event) => {
+    //console.log(event.target.value)
+    setSearchName(event.target.value)
+    countriesService.getAll()
+      .then(response => {
+        console.log(response)
+        //filter event.target.value
+      })
+      .catch(() => {
+        console.log('Something failed!')
+      })
+  }
 
   return (
     <>
       <p>
-        Find countries 
+        Find countries <input value={searchName} onChange={searchCountries} />
       </p>
     </>
   )
