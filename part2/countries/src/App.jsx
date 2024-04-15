c562770ac810700d4d003c29babbfc93e860623b
SisselJM <144883413+SisselJM@users.noreply.github.com>
2024-04-15 09:16:04 +0200

    Filter and show countries

--- part2/countries/src/App.jsx
+++ part2/countries/src/App.jsx
@@ -1,19 +1,50 @@
-import { useState } from 'react'
+import { useEffect, useState } from 'react'
 import countriesService from './services/countries'
+
+const CountriesList = (props) => {
+  //console.log(props)
+  //console.log(props.countries.length)
+  if (props.countries.length > 10) {
+    return (
+      <>
+        <p>Too many matches, specifiy another filter</p>
+      </>
+    )
+  }
+  return (
+    <ul>
+      {
+        props.countries.map(c =>          
+          <li key={c.name.common}>{c.name.common}</li>            
+        )
+      }
+    </ul>    
+  )
+}
 
 function App() {
   const [searchName, setSearchName] = useState('')
+  const [allCountries, setAllCountries] = useState([])
+  const [countries, setCountries] = useState([])
+
+  useEffect(() => {
+  countriesService.getAll()
+    .then(response => {
+      //console.log(response)
+      setAllCountries(response)
+      setCountries(response)
+    })
+    .catch(() => {
+      console.log('Something failed!')
+    })
+  }, [])
+
   const searchCountries = (event) => {
     //console.log(event.target.value)
     setSearchName(event.target.value)
-    countriesService.getAll()
-      .then(response => {
-        console.log(response)
-        //filter event.target.value
-      })
-      .catch(() => {
-        console.log('Something failed!')
-      })
+    const filteredCountries = allCountries.filter(x => x.name.common.startsWith(event.target.value))
+    //console.log(filteredCountries)
+    setCountries(filteredCountries)
   }
 
   return (
@@ -21,6 +52,7 @@ function App() {
       <p>
         Find countries <input value={searchName} onChange={searchCountries} />
       </p>
+      <CountriesList countries={countries} />
     </>
   )
 }
