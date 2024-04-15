867e374480de7070ae931f660098b7bde3b6c2dc
SisselJM <144883413+SisselJM@users.noreply.github.com>
2024-04-15 08:06:01 +0200

    2.18 countries init

---
+++ part2/countries/src/App.jsx
@@ -0,0 +1,15 @@
+import { useState } from 'react'
+
+function App() {
+  const [searchName, setSearchName] = useState('')
+
+  return (
+    <>
+      <p>
+        Find countries 
+      </p>
+    </>
+  )
+}
+
+export default App
