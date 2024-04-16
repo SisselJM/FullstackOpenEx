b579ca93ae359ed66715e966253cea8e5156b073
SisselJM <144883413+SisselJM@users.noreply.github.com>
2024-04-16 14:43:23 +0200

    Comment - not relevant

--- part3/backend/index.js
+++ part3/backend/index.js
@@ -1,1 +1,11 @@
-console.log('Hello world')
+//part3 is in another repository: https://github.com/SisselJM/phonebook - C:\Users\sisse\source\repos\React\phonebook
+const http = require('http')
+
+const app = http.createServer((request, response) => {
+    response.writeHead(200, { 'Content-Type': 'text/plain'})
+    response.end('Hello world')
+})
+
+const PORT = 3001
+app.listen(PORT)
+console.log(`Server running on port ${PORT}`)
