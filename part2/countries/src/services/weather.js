8b5bcb9ea6ebf394d7472d675c0c3e5530dc9052
SisselJM <144883413+SisselJM@users.noreply.github.com>
2024-04-18 15:05:57 +0200

    Move apikey to .env, gitignore

--- part2/countries/src/services/weather.js
+++ part2/countries/src/services/weather.js
@@ -1,9 +1,9 @@
 import axios from 'axios'
 const baseUrl = 'https://api.openweathermap.org/data/2.5/weather'
-const APIKEY = import.meta.env.APIKEY
+const APIKEY = process.env.MONGODB_URI
 
 const getByCity = (city) => {
-    const request = axios.get(baseUrl + `?q=${city}&APPID=${APIKEY}&units=metric`)
+    const request = axios.get(baseUrl + `?q=${city}&units=metric&APPID=${APIKEY}`)
     return request.then(response => response.data)    
   }
   
