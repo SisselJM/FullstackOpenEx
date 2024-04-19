2b0bf93e8303edd5a1da28f9de5f181d8faa32c3
SisselJM <144883413+SisselJM@users.noreply.github.com>
2024-04-19 11:36:57 +0200

    4.2 move to app

--- part4/bloglist/index.js
+++ part4/bloglist/index.js
@@ -1,17 +1,5 @@
-const express = require('express')
-const app = express()
-const cors = require('cors')
-const mongoose = require('mongoose')
+const app = require('./app')
 const config = require('./utils/config')
-const blogsRouter = require('./controllers/blogs')
-
-const mongoUrl = config.MONGODB_URI
-mongoose.connect(mongoUrl)
-
-app.use(cors())
-app.use(express.json())
-
-app.use('/api/blogs', blogsRouter)
 
 const PORT = config.PORT || 3003
 app.listen(PORT, () => {
