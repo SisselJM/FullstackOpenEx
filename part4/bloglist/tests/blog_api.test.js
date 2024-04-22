0e943e13480ce33780b058a04f8d08ddcb83c93a
SisselJM <144883413+SisselJM@users.noreply.github.com>
2024-04-22 14:13:55 +0200

    group

--- part4/bloglist/tests/blog_api.test.js
+++ part4/bloglist/tests/blog_api.test.js
@@ -8,70 +8,73 @@ const helper = require('./test_helper')
 
 const api = supertest(app)
 
-//4.8
-test('blogs are returned as json', async () => {
-  await api
-    .get('/api/blogs')
-    .expect(200)
-    .expect('Content-Type', /application\/json/)
-})
-
-test('there are 2 blogs', async () => {
-  const response = await api.get('/api/blogs')
-  assert.strictEqual(response.body.length, 2)
-})
-
-//4.9
-test('the unique identifier property of the blog posts is named id', async () => {
-    const response = await api.get('/api/blogs')
-    //console.log(response.body[0]) //
-    const id = response.body.map(r => r.id)[0]
-    assert.notStrictEqual(id, undefined)
+describe('when there is initially some items saved', () => {
+  beforeEach(async () => {
+    await Blog.deleteMany({})
+    let blog = new Blog(helper.initialBlogs[0])
+    await blog.save()
+    blog = new Blog(helper.initialBlogs[1])
+    await blog.save()
   })
 
-//4.10
-test('a valid blog can be added ', async () => {
+  //4.8
+  test('blogs are returned as json', async () => {
+    await api
+      .get('/api/blogs')
+      .expect(200)
+      .expect('Content-Type', /application\/json/)
+  })
+
+  test('there are 2 blogs', async () => {
+    const response = await api.get('/api/blogs')
+    assert.strictEqual(response.body.length, 2)
+  })
+  })
+
+describe('addition of a new note', () => {
+  //4.10
+  test('a valid blog can be added ', async () => {
     const newBlog = {
         title: "My second blog",
         author: "Newbie Blogger",
         url: "http://my.blog.no/2",
         likes: 2
     }
-  
+
     await api
       .post('/api/blogs')
       .send(newBlog)
       .expect(201)
       .expect('Content-Type', /application\/json/)
-  
-    const response = await api.get('/api/blogs')
-  
-    const title = response.body.map(r => r.title)
-  
-    assert.strictEqual(response.body.length, helper.initialBlogs.length + 1)
-  
-    assert(title.includes('My second blog'))
-})
 
-//4.11
-test('likes default to 0', async () => {
+    const response = await api.get('/api/blogs')
+
+    const title = response.body.map(r => r.title)
+
+    assert.strictEqual(response.body.length, helper.initialBlogs.length + 1)
+
+    assert(title.includes('My second blog'))
+  })
+
+  //4.11
+  test('likes default to 0', async () => {
     const newBlog = {
         title: "My second blog",
         author: "Newbie Blogger",
         url: "http://my.blog.no/2"
     }
-  
+
     const response = await api
       .post('/api/blogs')
       .send(newBlog)
       .expect(201)
       .expect('Content-Type', /application\/json/)
-  
+
     //console.log('body: ', response.body)
-  
+
     const likes = response.body.likes
     assert.strictEqual(likes, 0)
-})
+  })
 
 //4.12
 describe('missing request data, returns 400 Bad request', () => {
@@ -95,15 +98,16 @@ describe('missing request data, returns 400 Bad request', () => {
       })
       .expect(400)
   })
+  })
 })
-
-beforeEach(async () => {
-    await Blog.deleteMany({})
-    let blog = new Blog(helper.initialBlogs[0])
-    await blog.save()
-    blog = new Blog(helper.initialBlogs[1])
-    await blog.save()
 })
+//4.9
+test('the unique identifier property of the blog posts is named id', async () => {
+    const response = await api.get('/api/blogs')
+    //console.log(response.body[0]) //
+    const id = response.body.map(r => r.id)[0]
+    assert.notStrictEqual(id, undefined)
+  })
 
 after(async () => {
   await mongoose.connection.close()
