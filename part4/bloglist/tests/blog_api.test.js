3e764d9d2b7a1cdd3587083d58cd777191c5c271
SisselJM <144883413+SisselJM@users.noreply.github.com>
2024-04-22 11:04:15 +0200

    4.8 init db, test number og blogs

--- part4/bloglist/tests/blog_api.test.js
+++ part4/bloglist/tests/blog_api.test.js
@@ -1,8 +1,9 @@
-const { test, after } = require('node:test')
+const { test, after, beforeEach } = require('node:test')
 const assert = require('node:assert')
 const mongoose = require('mongoose')
 const supertest = require('supertest')
 const app = require('../app')
+const Blog = require('../models/blog')
 
 const api = supertest(app)
 
@@ -13,11 +14,33 @@ test('blogs are returned as json', async () => {
     .expect('Content-Type', /application\/json/)
 })
 
-test('there are 0 notes', async () => {
-    const response = await api.get('/api/blogs')
-  
-    assert.strictEqual(response.body.length, 0)
-  })
+test('there are 2 blogs', async () => {
+  const response = await api.get('/api/blogs')
+  assert.strictEqual(response.body.length, 2)
+})
+
+  const initalBlogs = [
+    {
+        title: "My first blog",
+        author: "Newbie Blogger",
+        url: "http://my.blog.no/1",
+        likes: 1
+    },
+    {
+        title: "My 123rd blog",
+        author: "Pro Blogger",
+        url: "http://pro.blog.no/123",
+        likes: 556
+    }
+  ]
+
+beforeEach(async () => {
+    await Blog.deleteMany({})
+    let blog = new Blog(initalBlogs[0])
+    await blog.save()
+    blog = new Blog(initalBlogs[1])
+    await blog.save()
+})
 
 after(async () => {
   await mongoose.connection.close()
