37410dea0a30a2c25c4ed5c12794d7443f8291b2
SisselJM <144883413+SisselJM@users.noreply.github.com>
2024-04-24 10:40:15 +0200

    Merge tests to 1 file

--- part4/bloglist/tests/blog_api.test.js
+++ part4/bloglist/tests/blog_api.test.js
@@ -4,11 +4,13 @@ const mongoose = require('mongoose')
 const supertest = require('supertest')
 const app = require('../app')
 const Blog = require('../models/blog')
+const User = require('../models/user')
 const helper = require('./test_helper')
+const bcrypt = require('bcrypt')
 
 const api = supertest(app)
 
-describe('when there is initially some items saved', () => {
+describe('when there is initially some items and one user saved', () => {
 
   beforeEach(async () => {
     await Blog.deleteMany({})
@@ -16,6 +18,11 @@ describe('when there is initially some items saved', () => {
     await blog.save()
     blog = new Blog(helper.initialBlogs[1])
     await blog.save()
+
+    await User.deleteMany({})
+    const passwordHash = await bcrypt.hash('sekret', 10)
+    const user = new User({ username: 'root', passwordHash })
+    await user.save()
   })
 
   //4.8
@@ -182,6 +189,83 @@ describe('when there is initially some items saved', () => {
       assert(!titles.includes(itemToDelete.title))
     })
   })
+
+  describe('create users', () => {
+    test('creation succeeds with a fresh username', async () => {
+      const usersAtStart = await helper.usersInDb()
+
+      const newUser = {
+        username: 'mluukkai',
+        name: 'Matti Luukkainen',
+        password: 'salainen',
+      }
+
+      await api
+        .post('/api/users')
+        .send(newUser)
+        .expect(201)
+        .expect('Content-Type', /application\/json/)
+
+      const usersAtEnd = await helper.usersInDb()
+      assert.strictEqual(usersAtEnd.length, usersAtStart.length + 1)
+
+      const usernames = usersAtEnd.map(u => u.username)
+      assert(usernames.includes(newUser.username))
+      console.log('Done')
+    })
+
+    test('create with too short password fails', async () => {
+      await api
+        .post('/api/users')
+        .send({
+          username: 'mikajo',
+          name: 'Mika Johnsen',
+          password: 'sa',
+        })
+        .expect(400)
+    })
+    
+    test('create with too short username fails', async () => {
+      await api
+      .post('/api/users')
+      .send({
+        username: 'Mi',
+        name: 'Mika Johnsen',
+        password: 'salainen',
+      })
+      .expect(400)
+  })
+  
+    test('create without username fails', async () => {
+        await api
+        .post('/api/users')
+        .send({
+          name: 'Mika Johnsen',
+          password: 'salainen',
+        })
+        .expect(400)
+    })
+//    
+  })
+
+  describe('retrieve users', () => {
+    test('users are returned as json', async () => {
+      await api
+        .get('/api/users')
+        .expect(200)
+        .expect('Content-Type', /application\/json/)
+    })
+
+    test('get users', async () => {
+      const response = await api
+        .get('/api/users')
+        .expect(200)
+        
+      //console.log('users: ', response.body.length)
+      assert.strictEqual(response.body.length, 1)
+    })
+  })
+
 })
 
 after(async () => {
