c5dd4f5384646993bc0c0579e59fafc4fac8da37
SisselJM <144883413+SisselJM@users.noreply.github.com>
2024-05-03 15:39:44 +0200

    5.17 wip

--- part5/bloglist-test/tests/bloglist.spec.js
+++ part5/bloglist-test/tests/bloglist.spec.js
@@ -1,9 +1,24 @@
-const { test, expect } = require('@playwright/test')
+const { test, expect, beforeEach, describe } = require('@playwright/test')
 
-test('front page can be opened', async ({ page }) => {
-  await page.goto('http://localhost:5173')
+describe('Blog app', () => {
+  beforeEach(async ({ page }) => {
+    await page.goto('http://localhost:5173')
+  })
 
-  const locator = await page.getByText('Blogs')
-  await expect(locator).toBeVisible()
-  //await expect(page.getByText('Note app, Department of Computer Science, University of Helsinki 2023')).toBeVisible()
+  test('front page can be opened', async ({ page }) => {
+    const locator = await page.getByText('Blogs')
+    await expect(locator).toBeVisible()
+    //await expect(page.getByText('Note app, Department of Computer Science, University of Helsinki 2023')).toBeVisible()
+  })
+
+  test('Login form is shown', async ({ page }) => {
+    await page.getByRole('button', { name: 'log in' }).click()
+    //feiler fordi denne brukeren er i prod, ikke test
+    await page.getByRole('textbox').first().fill('blogger1')
+    await page.getByRole('textbox').last().fill('12345678')
+    await page.getByRole('button', { name: 'login' }).click()
+  
+    await expect(page.getByText('The Best Blogger logged in')).toBeVisible()
+  })
+
 })
